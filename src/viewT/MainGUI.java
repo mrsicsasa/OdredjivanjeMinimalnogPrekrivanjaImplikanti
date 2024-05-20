bbd65753653e17d9341d8036d10008598b678a3c
LimboWRLD <namidzic34@gmail.com>
2024-05-20 21:12:39 +0200

    main window gui done

---
+++ src/viewT/MainGUI.java
@@ -0,0 +1,47 @@
+package viewT;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Kreiranje meni-a
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem saveMenuItem = new MenuItem("Save");
+        fileMenu.getItems().add(saveMenuItem);
+        menuBar.getMenus().add(fileMenu);
+
+        // Test primer, kasnije cemo dodati da se vrednosti dobijaju kroz api/funkcije
+        Label entryLabel = new Label("Entry Function: " +  " f(A,B,C,D) = Σm(0,2,5,7,8,10,12,13,15) ");
+        Label exitLabel = new Label("Exit Function: " + " f(A, B, C, D) = A'CD' + AB'D + BC'D' + ACD + BCD'");
+
+        // Centriranje labela 
+        HBox displays = new HBox(entryLabel, exitLabel);
+        displays.setSpacing(100);
+        displays.setAlignment(Pos.CENTER);
+        displays.setStyle("-fx-padding: 10;");
+
+        // Centriranje delova koji ce biti na sceni
+        VBox root = new VBox(menuBar, displays);
+        root.setAlignment(Pos.TOP_CENTER);
+
+        // Kreiranje scene
+        Scene scene = new Scene(root, 600, 400);
+
+        // Set Stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("K6");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
