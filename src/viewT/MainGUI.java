dac1c3e40527e93d38272a77b2c4224fa91a171c
LimboWRLD <namidzic34@gmail.com>
2024-05-21 20:02:27 +0200

    Added button for starting the operation

--- src/viewT/MainGUI.java
+++ src/viewT/MainGUI.java
@@ -19,8 +19,11 @@ public class MainGUI extends Application {
         menuBar.getMenus().add(fileMenu);
 
         // Test primer, kasnije cemo dodati da se vrednosti dobijaju kroz api/funkcije
-        Label entryLabel = new Label("Entry Function: " +  " f(A,B,C,D) = Σm(0,2,5,7,8,10,12,13,15) ");
+        Label entryLabel = new Label("Entry Function: " + " f(A,B,C,D) = Σm(0,2,5,7,8,10,12,13,15) ");
         Label exitLabel = new Label("Exit Function: " + " f(A, B, C, D) = A'CD' + AB'D + BC'D' + ACD + BCD'");
+
+        // Kreiranje dugmeta "Pocni"
+        Button pocniButton = new Button("Pocni");
 
         // Centriranje labela 
         HBox displays = new HBox(entryLabel, exitLabel);
@@ -28,8 +31,13 @@ public class MainGUI extends Application {
         displays.setAlignment(Pos.CENTER);
         displays.setStyle("-fx-padding: 10;");
 
+        // Kreiranje VBox-a za label-e i dugme
+        VBox content = new VBox(displays, pocniButton);
+        content.setSpacing(20);
+        content.setAlignment(Pos.CENTER);
+
         // Centriranje delova koji ce biti na sceni
-        VBox root = new VBox(menuBar, displays);
+        VBox root = new VBox(menuBar, content);
         root.setAlignment(Pos.TOP_CENTER);
 
         // Kreiranje scene
@@ -41,7 +49,4 @@ public class MainGUI extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
