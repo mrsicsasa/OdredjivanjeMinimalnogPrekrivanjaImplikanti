4443a4ea821f3b14c5f13f45702b553a9f0d9f5d
LimboWRLD <namidzic34@gmail.com>
2024-06-01 16:39:16 +0200

    Fixed .settings
    In .settings folder there was a copy of the entire branch

--- .settings/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module fdgf {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- .settings/src/view/Mode.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-import controller.MainController;
-import javafx.scene.layout.Pane;
-
-public interface Mode {
-    void updateUI(Pane root, MainController controler);
-}
